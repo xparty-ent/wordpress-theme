5c5ecfc739cc0eee40c9402bc394463b5ed65624
Matteo Murante <m.murante@x-party.it>
2023-10-18 22:04:45 +0200

    custom update function

--- functions.php
+++ functions.php
@@ -54,7 +54,7 @@ if (! function_exists('\Roots\bootloader')) {
 |
 */
 
-collect(['setup', 'filters', 'routes', 'menus', 'pages'])
+collect(['setup', 'filters', 'routes', 'menus', 'pages', 'update'])
     ->each(function ($file) {
         if (! locate_template($file = "app/{$file}.php", true, true)) {
             wp_die(
